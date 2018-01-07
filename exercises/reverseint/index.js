dc16ddaf4bb39e94fa5822232ecc332f2a88cc2b
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-08 00:47:17 +1100

    Clean up solution to reverse int (part 2)

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,18 +9,13 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-  let negative = false
-  // If n is negative, set negative to true
-  if (Math.sign(n) === -1) {
-    negative = true
-    n *= -1 // Temporarily convert n to positive
-  }
   // Convert n to string and reverse the string
-  reversed = reverse(n.toString())
-  // Convert the reversed string to number
-  return negative === false
-    ? parseInt(reversed) // Return as positive value
-    : parseInt(reversed) * -1 // Return as negative value
+  const reversed = reverse(n.toString())
+
+  // Convert the reversed string to number and multiply to it's sign
+  // Parsing to int will remove the sign in the end e.g. 5- becomes 5
+  // Math.sign produces -1 if the number is negative, 1 if the number is position and 0 for zero
+  return parseInt(reversed) * Math.sign(n)
 }
 
 // Reverse a string - Using array reverse()
