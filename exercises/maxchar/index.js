08485e17a1f3083f18a545d6bbe84250f3e9ea29
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-27 09:06:07 +1100

    Add solution to maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // Convert String to Object with count for each character
+  const characters = {}
+  str.split('').map( c => {
+    characters[c] = !!characters[c] ? characters[c] + 1 : 1
+  })
+  
+  // Find the character with the biggest value in the object
+  let mostCommonChar = ''
+  let highestOccurence = 0
+  for(let key in characters) {
+    // If the object value is highesr than the highest occurence,
+    // use object value as new highest occurence and have the key as the most common character
+    if (characters[key] > highestOccurence){
+      highestOccurence = characters[key]
+      mostCommonChar = key
+    }
+  }
+  
+  return mostCommonChar
+}
 
 module.exports = maxChar;
