f369fe8fc28b2e425d8967cbe0daa356ad99c155
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-27 09:32:32 +1100

    Add final solution to maxChar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -7,27 +7,29 @@
 
 function maxChar(str) {
   // Convert String to Object with count for each character
-  const chars = convertStringToCharMap(str)
-  
-  // Find the character with the biggest value in the object
-  let mostCommonChar = ''
-  let highestOccurence = 0
-  for(let key in chars) {
-    // If the object value is highesr than the highest occurence,
-    // use object value as new highest occurence and have the key as the most common character
-    if (chars[key] > highestOccurence){
-      highestOccurence = chars[key]
-      mostCommonChar = key
+  const charMap = convertStringToCharMap(str)
+
+  // Find the character which is most frequently used
+  let maxChar = ''
+  let max = 0
+  for(let char in charMap) {
+    // If the char value is highesr than the max char
+    // use char value as new max char
+    if (charMap[char] > max){
+      max = charMap[char]
+      maxChar = char
     }
   }
-  
-  return mostCommonChar
+  // Better, using reduce
+  // maxChar = Object.keys(charMap).reduce((a, b) => charMap[a] > charMap[b] ? a : b);
+
+  return maxChar
 }
 
 // Convert String to Object with count for each character
 const convertStringToCharMap = (str) => {
   const chars = {}
-  for(let char of str) {
+  for (let char of str) {
     // Using Ternary
     // chars[char] = !chars[char] ? 1 : chars[char] + 1
     // Using OR
@@ -43,7 +45,7 @@ const convertStringToCharMap = (str) => {
 //   str.split('').map( c => {
 //     chars[c] = !!chars[c] ? chars[c] + 1 : 1
 //   })
-  
+
 //   // Find the character with the biggest value in the object
 //   let mostCommonChar = ''
 //   let highestOccurence = 0
@@ -55,7 +57,7 @@ const convertStringToCharMap = (str) => {
 //       mostCommonChar = key
 //     }
 //   }
-  
+
 //   return mostCommonChar
 // }
 
