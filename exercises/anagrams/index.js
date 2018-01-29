d41d68e7a5dab35d0450208ef6df1fa93daeb3b7
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-29 13:39:16 +1100

    Add solution to anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // Function which creates a character map from a string
+  const charMapper = str => {
+    return str.split('').reduce((charMap, char) => {
+      if(!charMap[char]) charMap[char] = 1
+      else charMap[char]++
+      return charMap
+    }, {})
+  }
+
+  // Function which converts a string to only alphanumeric and lowercase characters
+  const alphanumericLowercase = str => {
+    return str.replace(/[^\w]/g,'').toLowerCase()
+  }
+  
+  // Create character map for string A and B
+  const charMapA = charMapper(alphanumericLowercase(stringA))
+  const charMapB = charMapper(alphanumericLowercase(stringB))
+
+  // If length of two character maps are equal
+  if(Object.keys(charMapA).length === Object.keys(charMapB).length) {
+    // For each key in char map A
+    for(let key in charMapA) {
+      // Check if key value is similar between char map A and B
+      if(charMapA[key] !== charMapB[key]) {
+        // If not, return false immediately
+        return false
+      }
+    }
+    // Return true if no inequality is found
+    return true
+  }
+  // Return false when two character maps are not equal
+  return false
+}
 
 module.exports = anagrams;
