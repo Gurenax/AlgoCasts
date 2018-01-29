a4bc67628e7a530a40be1d00cff1c4eeb9847738
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-29 12:27:16 +1100

    Add solution to chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // Create an empty array called chunked
+  const chunked = []
+  // Function to retrieve last element of array
+  const lastElementOfArray = arr => arr[arr.length-1]
+
+  // Iterate over the original array
+  for(num of array) {
+    // Get last element of chunked
+    const lastElem = lastElementOfArray(chunked)
+    // If the last element is undefined or its length is equal to chunk size
+    if(!lastElem || lastElem.length===size) {
+      // Push a new array containing the number in chunked
+      chunked.push([num])
+    }
+    else {
+      // Push the number to the last element
+      lastElem.push(num)
+    }
+  }
+
+  return chunked
+}
+
 
 module.exports = chunk;
